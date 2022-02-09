2989cf82a222f8e3c0b46f2bfb84438a7513671c
Carlos Damken <carlos@damken.com>
2022-02-09 12:11:04 +0100

    Update get_app_version.sh
    you have to search version instead description

--- get_app_version.sh
+++ get_app_version.sh
@@ -4,4 +4,4 @@
 
 apt instal xmlstartlet
 cd /var/www/owncloud/apps
-find . -type f -name info.xml -exec echo {} \; -exec xmlstarlet select --template --value-of /info/description --nl {} \;
+find . -type f -name info.xml -exec echo {} \; -exec xmlstarlet select --template --value-of /info/version --nl {} \;

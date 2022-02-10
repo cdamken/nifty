76f29b3f5d4a2275a17961b7f6cbd812946949dd
Dmitry Mayorov <voroyam@owncloud.com>
2022-02-10 09:22:54 +0100

    added 2 variations of the commands

--- get_app_version.sh
+++ get_app_version.sh
@@ -4,4 +4,8 @@
 
 apt instal xmlstartlet
 cd /var/www/owncloud/apps
+
+# show app descriptions
 find . -type f -name info.xml -exec echo {} \; -exec xmlstarlet select --template --value-of /info/description --nl {} \;
+# show app versions
+find . -type f -name info.xml -exec echo {} \; -exec xmlstarlet select --template --value-of /info/version --nl {} \;
